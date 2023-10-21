d92d01428340543cbbd4c4bf988c8e2a8f58b588
Oliver Wu <z5379238@ad.unsw.edu.au>
2023-10-21 16:53:33 +1100

    feat: frontend init

--- backend/quiz/views.py
+++ backend/quiz/views.py
@@ -1,7 +1,7 @@
 from rest_framework import generics
 from rest_framework.response import Response
-from quiz.models import Quizzes, Question
-from quiz.serializers import QuizSerializer, RandomQuestionSerializer, QuestionSerializer
+from backend.quiz.models import Quizzes, Question
+from backend.quiz.serializers import QuizSerializer, RandomQuestionSerializer, QuestionSerializer
 from rest_framework.views import APIView
 
 
