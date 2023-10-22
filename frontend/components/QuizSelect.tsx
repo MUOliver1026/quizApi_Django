8865559b7a3251c25aec218b9ed5fb0fbf5b2846
Oliver Wu <z5379238@ad.unsw.edu.au>
2023-10-23 00:32:25 +1100

    feat: change frontend structure

--- frontend/components/QuizSelect.tsx
+++ frontend/components/QuizSelect.tsx
@@ -9,7 +9,7 @@ type Quiz = {
     title: string;
 };
 
-const API_URL = "https://oliverwu.pythonanywhere.com//quiz/";
+const API_URL = "https://oliverwu.pythonanywhere.com/quiz/";
 
 const QuizSelect = () => {
     const [dataState] = ConnectApi(API_URL);
