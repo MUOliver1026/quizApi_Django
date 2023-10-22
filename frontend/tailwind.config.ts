5fa404f8d5ec54c1124b1bf2f1217ddb740e79ed
Oliver Wu <z5379238@ad.unsw.edu.au>
2023-10-23 00:35:44 +1100

    feat: change frontend structure

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -56,17 +56,17 @@ const config: Config = {
         sm: "calc(var(--radius) - 4px)",
       },
       fontFamily: {
-        sans: ["var(--font-sans)", ...fontFamily.sans],
+        // sans: ["var(--font-sans)", ...fontFamily.sans],
       },
       keyframes: {
-        "accordion-down": {
-          from: { height: 0 },
-          to: { height: "var(--radix-accordion-content-height)" },
-        },
-        "accordion-up": {
-          from: { height: "var(--radix-accordion-content-height)" },
-          to: { height: 0 },
-        },
+        // "accordion-down": {
+        //   from: { height: 0 },
+        //   to: { height: "var(--radix-accordion-content-height)" },
+        // },
+        // "accordion-up": {
+        //   from: { height: "var(--radix-accordion-content-height)" },
+        //   to: { height: 0 },
+        // },
       },
       animation: {
         "accordion-down": "accordion-down 0.2s ease-out",
